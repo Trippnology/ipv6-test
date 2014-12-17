4cbdd7dfbf2b3a3fd4aecb84a85f06f65cc5dd10
Trippnology <trippnology@gmail.com>
2014-12-17 16:06:58 +0000

    Outputting IP address in various formats.

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
-<?php $addr = $_SERVER['REMOTE_ADDR']; ?>
+<?php
+	// Get visitors real IP when using Cloudflare
+	if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+		$_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+	}
+	$userIP = $_SERVER['REMOTE_ADDR'];
+	$format = $_GET['format'];
 
+	/* If the "format" query parameter is used, simply return the IP
+	   and forget about rendering the rest of the page */
+	if ($format == "text") {
+		header('Content-Type: text/plain; charset=utf8');
+		echo $userIP;
+		die;
+	}
+	if ($format == "json") {
+		header('Content-Type: application/json; charset=utf8');
+		echo json_encode(array('ip' => $userIP));
+		die;
+	}
+	if ($format == "jsonp") {
+		header('Content-Type: text/javascript; charset=utf8');
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Max-Age: 3628800');
+		header('Access-Control-Allow-Methods: GET');
+		$callback = $_GET['callback'];
+		echo $callback . '(' . json_encode(array('ip' => $userIP)) . ');';
+		die;
+	}
+?>
 <!DOCTYPE html>
 <html lang="en-GB">
 <head>
@@ -10,6 +38,8 @@
 	<style>
 		body { text-align: center; }
 		img[alt="IPv6 logo"] { margin-bottom: 10px; }
+		.help { text-align: left; }
+		.help .lead { text-align: center; }
 	</style>
 </head>
 
@@ -23,19 +53,30 @@
 
 		<main role="main">
 			<div class="row">
-				<article>
+				<div class="col-sm-12">
 					<p>If you're reading this, you can access 'IPv6 only' sites as this site does not have an A record, only an AAAA record.</p>
 					<h2>Your address is:</h2>
-					<p class="ip"><?=$addr?></p>
+					<p class="ip"><?=$userIP?></p>
 					<img src="img/ipv6-logo.png" alt="IPv6 logo">
-				</article>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-offset-3 col-md-6 help">
+					<p class="lead">Help</p>
+					<p>You can add a <code>format</code> query parameter to get your IP in various formats, like so:</p>
+					<ol>
+						<li>Text: <code>http://ipv6.trippnology.com/?format=text</code></li>
+						<li>JSON: <code>http://ipv6.trippnology.com/?format=json</code></li>
+						<li>JSONP: <code>http://ipv6.trippnology.com/?format=jsonp&amp;callback=yourcallback</code></li>
+					</ol>
+				</div>
 			</div>
 		</main>
 
 		<footer role="contentinfo">
 			<div class="row">
 				<p>Built with <a href="http://github.com/Trippnology/Matchbox">Matchbox</a> from <a href="http://trippnology.com/">Trippnology</a></p>
-				<img src="img/ipv6-80-15.png" alt="IPv6 ready">
+				<img src="img/ipv6-80x15.png" alt="IPv6 ready">
 			</div>
 		</footer>
 	</div>
